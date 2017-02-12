5e158b430cc08ca5b725f58acf9c29b41302677c
elver <elver@192.168.1.75>
2017-02-12 15:21:46 -0600

    Tarea 2 problema 1

--- src/spp/ejimenezr/t01/p1/SPPEJimenezRT01P1.java
+++ src/spp/ejimenezr/t01/p1/SPPEJimenezRT01P1.java
@@ -19,48 +19,56 @@ public class SPPEJimenezRT01P1 {
         double sue, he, sF;
         Scanner kb =new Scanner(System.in);
         
-            System.out.println("Introduce una de las 5 categorías");
+            System.out.println("Introduce una de las 8 categorías");
         cate=kb.nextInt();
-        if (cate>=1 && cate<=5){
+        if (cate>=1 && cate<=8){
         switch (cate){
             case 1:
-                System.out.println("Introduzca las horas extra");
-                he=kb.nextDouble();
-                System.out.println("Introduzca el sueldo base");
-                sue=kb.nextDouble();  
+                he= mensajePedirHorasExtra();
+                sue= mensajePedirSueldo();
                 sF=sue+(he*30);
                 System.out.println("El sueldo final es "+sF);
                 break;
             case 2:
-                System.out.println("Introduzca las horas extra");
-                he=kb.nextDouble();
-                System.out.println("Introduzca el sueldo base");
-                sue=kb.nextDouble(); 
+                he= mensajePedirHorasExtra();
+                sue= mensajePedirSueldo();
                 sF=sue+(he*38);
                 System.out.println("El sueldo final es "+sF);
                 break;
             case 3:
-                System.out.println("Introduzca las horas extra");
-                he=kb.nextDouble();
-                System.out.println("Introduzca el sueldo base");
-                sue=kb.nextDouble(); 
+                he= mensajePedirHorasExtra();
+                sue= mensajePedirSueldo();
                 sF=sue+(he*50);
                 System.out.println("El sueldo final es "+sF);
                 break;
             case 4:
-                System.out.println("Introduzca las horas extra");
-                he=kb.nextDouble();
-                System.out.println("Introduzca el sueldo base");
-                sue=kb.nextDouble(); 
+                he= mensajePedirHorasExtra();
+                sue= mensajePedirSueldo();
                 sF=sue+(he*70);
                 System.out.println("El sueldo final es "+sF);
                 break;
             case 5:
-                System.out.println("Introduzca las horas extra");
-                he=kb.nextDouble();
-                System.out.println("Introduzca el sueldo base");
-                sue=kb.nextDouble(); 
+                he= mensajePedirHorasExtra();
+                sue= mensajePedirSueldo();
                 sF=sue+(he*90);
+                System.out.println("El sueldo final es "+sF);
+                break;
+            case 6:
+                he= mensajePedirHorasExtra();
+                sue= mensajePedirSueldo();
+                sF=sue+(he*110);
+                System.out.println("El sueldo final es "+sF);
+                break;
+            case 7:
+                he= mensajePedirHorasExtra();
+                sue= mensajePedirSueldo();
+                sF=sue+(he*130);
+                System.out.println("El sueldo final es "+sF);
+                break;
+            case 8:
+                he= mensajePedirHorasExtra();
+                sue= mensajePedirSueldo();
+                sF=sue+(he*150);
                 System.out.println("El sueldo final es "+sF);
                 break;
             }
@@ -69,4 +77,19 @@ public class SPPEJimenezRT01P1 {
             System.out.println("Valor no admitido");
                 }
         }
+    static double mensajePedirHorasExtra() {
+        Scanner kb= new Scanner(System.in);
+        double he;
+            System.out.println("Introduzca las horas extra");
+            he=kb.nextDouble();
+            return he;
+    }
+    static double mensajePedirSueldo() {
+        double sue;
+        Scanner kb= new Scanner(System.in);
+        System.out.println("Introduzca el sueldo inicial");
+        sue=kb.nextDouble();
+        return sue;
+        
+    }
 }
